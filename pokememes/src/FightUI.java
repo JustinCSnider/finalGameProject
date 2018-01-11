6c43659170e6b7fa12f25c6d48c97ba7efa39142
Jake <jdkemp1@gmail.com>
2018-01-11 13:40:18 -0700

    added platforms and a place to put names and HP for fight UI added oval class

--- pokememes/src/FightUI.java
+++ pokememes/src/FightUI.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -9,8 +10,9 @@ import javax.swing.JLabel;
 public class FightUI {
 public JFrame win;
 
+
 	public FightUI() {
-		win = new JFrame("fight menu ");
+		win = new JFrame("fight menu");
 	      win.setLayout(null);
 	      win.setBounds(0,0,650,600);
 	      
@@ -19,38 +21,91 @@ public JFrame win;
 	      b.setBackground(Color.gray);
 	      win.add(b,0);
 	      
-	     
+	      JLabel event;
+	      event = new JLabel();
+	      event.setText(" Is it working?");     
+	      event.setBounds(10,470,300,50);
+	      b.add(event,0);
+	      
+	      Rectangle box = new Rectangle(10,290,100,40);
+	      box.setBackground(Color.white);
+	      b.add(box,0);
+	      
+	      Rectangle boxe = new Rectangle(420,10,100,40);
+	      boxe.setBackground(Color.white);
+	      b.add(boxe,0);
+	      
+	      JLabel enemyHP;
+	     enemyHP = new JLabel();
+	      enemyHP.setText("EnemyHP:" );//add Hp
+	      enemyHP.setBounds(5,5,100,50);
+	      boxe.add(enemyHP,0);
+	      
+	      JLabel enemyName;
+	      enemyName = new JLabel();
+	      enemyName.setText("ChangeLater");
+	    		  enemyName.setBounds(5,-15,100,50);
+		      boxe.add(enemyName,0);
+		      
+		      JLabel heroName;
+		      heroName = new JLabel();
+		      heroName.setText("ChangeLater");
+		      heroName.setBounds(5,-15,100,50);
+			      box.add(heroName,0);
+	      
+	      JLabel heroHP;
+	     heroHP = new JLabel();
+	      heroHP.setText("HeroHP:" ); //add Hp
+	      heroHP.setBounds(5,5,100,50);
+	      box.add(heroHP,0);
+	      
+	      Oval standy;
+	      standy = new Oval(70,370,150,90);
+	      standy.setBackground(Color.white);
+	      b.add(standy,0);
+	      
+	      Oval standy2;
+	      standy2 = new Oval(470,90,150,90);
+	      standy2.setBackground(Color.white);
+	      b.add(standy2,0);
 	      
 	      JButton attack;
 	      attack = new JButton("Attack");
 	      attack.setBounds(10,520,300,50);
+	      attack.addActionListener(new ActionListener() {
+	    	  public void actionPerformed(ActionEvent e) {
+	    		  
+	    	  }
+	      });
 	      b.add(attack,0);
 	      
 	      JButton run;
 	      run = new JButton("Run");
 	      run.setBounds(340,520,300,50);
+	      run.addActionListener(new ActionListener() {
+	    	  public void actionPerformed(ActionEvent e) {
+	    		  
+	    	  }
+	      });
 	      b.add(run,0);
 	      
 	      JButton catch1;
 	      catch1 = new JButton("Catch");
 	      catch1.setBounds(340,470,300,50);
+	      catch1.addActionListener(new ActionListener() {
+	    	  public void actionPerformed(ActionEvent e) {
+	    		 
+	    	  }
+	      });
 	      b.add(catch1,0);
 	      
-	      JLabel event;
-	      event = new JLabel("Blah blah");
-	      event.setLocation(10,470);
-	      event.setLayout(null);
-	      b.add(event,0);
-	      event.setVisible(true);
+	      
+	      
 	      
 	      
 	      win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	      win.setVisible(true);
-	      attack.addActionListener(new ActionListener() {
-	    	  public void actionPerformed(ActionEvent e) {
-	    		  //add what is called
-	    	  }
-	      });
+	      
 
 	}
 
--- pokememes/src/UI.java
+++ pokememes/src/UI.java
@@ -93,7 +93,7 @@ public class UI extends EzImage
     		  win.dispose();
       	  }
         });
-       
+      
 	}
 	
 
